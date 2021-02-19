46667404965603adb735c780fbdee5d40d66f55e
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-02-18 21:02:33 -0500

    extract link from html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-const fsLibrary = require('fs');
+const fs = require('fs');
 const getHTML = require('./util/getHTML');
+const getLinks = require('./util/getLinks');
 require('dotenv').config();
-let html;
 
 (async () => {
-	if (process.env.NODE_ENV == 'test')
-		fsLibrary.readFile('html.txt', 'utf8', (err, data) => (html = data));
-	else html = await getHTML();
-	fsLibrary.writeFile('html.txt', html, () => console.log('written to file'));
+	if (process.env.NODE_ENV === 'test') {
+		console.log('hey');
+		fs.readFile('html.txt', 'utf8', (err, html) => {
+			const links = getLinks(html);
+		});
+	} else {
+		html = await getHTML();
+		fs.writeFile('html.txt', html, () => console.log('written to file'));
+		// const links = getLinks(html);
+	}
 })();
---
+++ src/util/getLinks.js
@@ -0,0 +1,18 @@
+const { parse } = require('node-html-parser');
+const getLinks = (html) => {
+	let links = [];
+	while (html.search('<a') != -1) {
+		const startIndex = html.search('<a');
+		const endIndex = html.search('</a>') + 4;
+		const linkTag = html.substring(startIndex, endIndex);
+		const linkEle = parse(linkTag).childNodes[0];
+		// console.log(linkEle);
+		const href = linkEle.getAttribute('href');
+		const text = linkEle.textContent;
+		links.push({ href, text });
+		html = html.substring(endIndex);
+	}
+	return links;
+};
+
+module.exports = getLinks;
