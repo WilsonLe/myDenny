1ec0f9f4a9d3db8744d5e7e8e672077dbc666c9b
Wilson <43991506+WilsonLe@users.noreply.github.com>
2021-03-11 19:41:29 -0500

    more logger fix:

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,12 +4,11 @@ require('dotenv').config();
 
 const logger = winston.createLogger({
 	levels: winston.config.syslog.levels,
-	transports: [new winston.transports.Syslog()],
+	format: winston.format.simple(),
+	transports: [new winston.transports.Console()],
 });
 
-//
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
 
 module.exports = logger;
