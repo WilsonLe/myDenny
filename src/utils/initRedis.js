b69401e307387eb4179065c900fe4c4f633e3cf0
WilsonLe <leanhminh2907@gmail.com>
2021-03-14 22:12:49 -0400

    change env key

--- src/utils/initRedis.js
+++ src/utils/initRedis.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const redisUri = process.env.REDIS_URI;
+const redisUri = process.env.REDIS_URL;
 const redis = require('async-redis').createClient({ url: redisUri });
 const logger = require('./logger');
 
